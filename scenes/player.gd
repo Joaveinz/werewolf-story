9555f7b177d183a446ad19320a4430769bcb135b
julo-web-dev <julianstaiger98@gmail.com>
2025-04-15 10:16:05 +0200

    wip: add muliplayer

--- scenes/player.gd
+++ scenes/player.gd
@@ -12,40 +12,44 @@ var health = MAX_HEALTH
 var invincible = false
 var invincibility_time = 0.1  # Seconds of invincibility after taking damage
 
+func _enter_tree() -> void:
+	set_multiplayer_authority(name.to_int())
+
 func _ready():
 	health_bar.max_value = MAX_HEALTH
 	health_bar.value = health
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if is_multiplayer_authority():
+		if not is_on_floor():
+			velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
-	if Input.is_action_just_pressed("ui_cancel"):
-		# game turn off
-		get_tree().quit()
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			
+		if Input.is_action_just_pressed("ui_cancel"):
+			# game turn off
+			get_tree().quit()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		# Set speed based on whether running or walking
-		var current_speed = RUN_SPEED if Input.is_action_pressed("ui_shift") else WALK_SPEED
-		velocity.x = direction * current_speed
-		# Play running animation when moving and holding shift, otherwise walk
-		if Input.is_action_pressed("ui_shift"):
-			animated_sprite.play("run")
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("ui_left", "ui_right")
+		if direction:
+			# Set speed based on whether running or walking
+			var current_speed = RUN_SPEED if Input.is_action_pressed("ui_shift") else WALK_SPEED
+			velocity.x = direction * current_speed
+			# Play running animation when moving and holding shift, otherwise walk
+			if Input.is_action_pressed("ui_shift"):
+				animated_sprite.play("run")
+			else:
+				animated_sprite.play("walk")
+			# Flip the sprite based on direction
+			animated_sprite.flip_h = direction < 0
 		else:
-			animated_sprite.play("walk")
-		# Flip the sprite based on direction
-		animated_sprite.flip_h = direction < 0
-	else:
-		velocity.x = move_toward(velocity.x, 0, RUN_SPEED)
-		# Play idle animation when not moving
-		animated_sprite.play("idle")
+			velocity.x = move_toward(velocity.x, 0, RUN_SPEED)
+			# Play idle animation when not moving
+			animated_sprite.play("idle")
 
 	move_and_slide()
 
