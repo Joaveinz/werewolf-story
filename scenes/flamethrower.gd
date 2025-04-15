a349ff421d380dcd1f2a816649a2b24fd7596b76
julo-web-dev <julianstaiger98@gmail.com>
2025-04-15 16:04:31 +0200

    flamethrower should only be able to be fired for the current player

--- scenes/flamethrower.gd
+++ scenes/flamethrower.gd
@@ -1,12 +1,30 @@
 extends Node2D
 
 @onready var fire = $fireEffect
+@onready var player = get_parent()
 
 var direction = -1
 var shooting = false
 
 func _process(delta):
-	look_at(get_global_mouse_position())
+	if not player.is_multiplayer_authority():
+		fire.emitting = false
+		return
+		
+	# Get mouse position relative to player
+	var mouse_pos = get_global_mouse_position()
+	var player_pos = player.global_position
+	
+	# Calculate direction to mouse
+	var direction_to_mouse = mouse_pos.x - player_pos.x
+	
+	# Only allow firing in the direction the player is facing
+	if (direction_to_mouse > 0 and player.animated_sprite.flip_h) or \
+	   (direction_to_mouse < 0 and not player.animated_sprite.flip_h):
+		fire.emitting = false
+		return
+		
+	look_at(mouse_pos)
 	rotation_degrees = wrap(rotation_degrees, 0, 360)
 	
 	if rotation_degrees > 90 and rotation_degrees < 270:
